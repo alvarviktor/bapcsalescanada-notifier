a0623ef74c5d75ec5b02a354614904f3a795b32a
alvarviktor <viktor.alvar@yahoo.com>
2020-10-25 11:29:32 -0700

    scraped timestamp, product page, and reddit page links

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ def main():
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find_all('div', class_ = 'scrollerItem', id = re.compile('^(t3_(.){6}$)'))
 
+    # Extract data from each reddit post
     for result in results:
-        print(result.find('h3').text)
+        post_title = result.find('h3').text
+        post_timestamp = result.find('a', attrs = {'data-click-id': 'timestamp'})
+        post_product_page = result.find('a', class_ = 'styled-outbound-link').attrs['href']
+        post_reddit_page = post_timestamp.attrs['href']
+
+        print("title: {}\ntimestamp: {}\nproduct page: {}\nreddit page: {}\n"
+        .format(post_title, post_timestamp.text, post_product_page, post_reddit_page))
 
 if __name__ == '__main__':
     main()
